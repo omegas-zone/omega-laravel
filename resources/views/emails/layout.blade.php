84d39e3b2777361938e2cbc9f4fc652d32220741
Swimmer <11476014+swimmer-zone@users.noreply.github.com>
2026-09-14 17:33:04 +0200

    Fix dark mode

--- resources/views/emails/layout.blade.php
+++ resources/views/emails/layout.blade.php
@@ -62,6 +62,7 @@
                 background-color: #222222 !important;
                 border-color: #444444 !important;
                 border-radius: 8px;
+                box-shadow: 0 4px 12px rgba(0, 0, 0, 0.5) !important;
             }
             .email-heading {
                 color: #eeeeee !important;
